31b7c36523b4b7dc5d589b124d32d4999ab4e2a6
Tomas Gomez <tomasgomezpassadore@gmail.com>
2022-10-01 16:17:26 -0300

    listar pencas compartidas

--- BackEnd/Controllers/AdminController.cs
+++ BackEnd/Controllers/AdminController.cs
@@ -17,10 +17,11 @@ namespace BackEnd.Controllers
         [HttpGet("{id}")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        public async Task<IActionResult> GetById(int id) { 
+        public async Task<IActionResult> GetById(int id)
+        {
 
             var admin = await _context.Administradores.FindAsync(id);
-            if(admin == null) return BadRequest("No existe el administrador");
+            if (admin == null) return BadRequest("No existe el administrador");
 
             DtAdmin dtAdmin = new DtAdmin();
             dtAdmin.Name = admin.nombre;
@@ -36,7 +37,8 @@ namespace BackEnd.Controllers
         [HttpPost("altaAdmin")]
         [ProducesResponseType(StatusCodes.Status201Created)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        public async Task<IActionResult> altaAdmin(DtAdmin dtAdmin) {
+        public async Task<IActionResult> altaAdmin(DtAdmin dtAdmin)
+        {
 
             DbSet<Usuario> usuarios = _context.Usuario;
             DbSet<Empresa> empresas = _context.Empresas;
@@ -74,6 +76,61 @@ namespace BackEnd.Controllers
 
             return Ok(dtAdmin);
         }
-    }
+        [HttpPut("agregarPenca/{idAdmin}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> agregarPenca(int idADmin, int idPenca)
+        {
 
+            var penca = await _context.Pencas.FindAsync(idPenca);
+            if (penca == null) return BadRequest("No existe la penca");
+
+            var admin = await _context.Administradores.FindAsync(idADmin);
+            if (admin == null) return BadRequest("No existe el administrador");
+
+            if (admin.pencas == null)
+            {
+                admin.pencas = new List<Penca>();
+            }
+            admin.pencas.Add(penca);
+
+            _context.Entry(admin).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+
+        }
+        [HttpGet("PencasCompartidasGet/{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> listarPencasCompartidas(int id)
+        {
+
+            var admin = _context.Administradores.Include(a => a.pencas);
+            List<DtPencasCompartida> pencasComp = new List<DtPencasCompartida>();
+            foreach (var a in admin)
+            {
+                if (a.Id == id)
+                {
+
+                    foreach (var aux in a.pencas)
+                    {
+                        DtPencasCompartida pencasaCompartidas = new DtPencasCompartida();
+                        pencasaCompartidas.id = aux.id;
+                        pencasaCompartidas.nombre = aux.nombre;
+                        pencasaCompartidas.tipoDeporte = aux.tipo_Deporte;
+                        pencasaCompartidas.entrada = aux.entrada;
+                        pencasaCompartidas.pozo = aux.pozo;
+                        pencasComp.Add(pencasaCompartidas);
+                    }
+                    return Ok(pencasComp);
+                }
+                
+            }
+            return NotFound();
+        }
+    }
 }
+
+
