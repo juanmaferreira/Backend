b7edd0ee1e03f178e318e33e6ed8ae71f5a017e6
Gonzalo Cánepa <69828698+Gon2812@users.noreply.github.com>
2022-10-13 22:06:09 -0300

    pull
    Solo quiero hacer un pull T-T

---
+++ BackEnd/Models/DataTypes/DtChat.cs
@@ -0,0 +1,14 @@
+﻿using BackEnd.Models.Clases;
+
+namespace BackEnd.Models.DataTypes
+{
+    public class DtChat
+    {
+        public int Id { get; set; }
+
+        public string usuario { get; set; }
+
+        public string empresa { get; set; }
+
+    }
+}
---
+++ BackEnd/Models/DataTypes/DtPartido.cs
@@ -0,0 +1,20 @@
+﻿using BackEnd.Models.Clases;
+
+namespace BackEnd.Models.DataTypes
+{
+    public class DtPartido
+    {
+        public int Id { get; set; }
+        public DateTime fecha   { get; set; }
+
+        public int Idlocal { get; set; }
+
+        public int Idvisitante { get; set; }
+
+        public string local { get; set; }
+
+        public string visitante { get; set; }   
+
+        public Tipo_Resultado resultado { get; set; }
+    }
+}
